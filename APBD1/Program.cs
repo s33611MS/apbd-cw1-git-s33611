174f9163b00d6befcd9bb3185c310b9ab88f5c9c
s33611MS <s33611@pjwstk.edu.pl>
2026-03-16 16:21:13 +0100

    Deleted Fun method

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,15 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
-
-void Fun(int[] arr)
-{
-    foreach (var v in arr)
-    {
-        Console.WriteLine(v);
-    }
-}
-
 int[] arr = new [] { 1, 2, 3, 4, 5 };
-Fun(arr);
 
 int ArrSum(int[] arr)
 {
