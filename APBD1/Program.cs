381d6983e3f09cb6dc229a2289825a6124b85e27
s33611MS <s33611@pjwstk.edu.pl>
2026-03-16 16:19:18 +0100

    Added array summing method

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 
-void fun(int[] arr)
+void Fun(int[] arr)
 {
     foreach (var v in arr)
     {
@@ -9,4 +9,17 @@ void fun(int[] arr)
 }
 
 int[] arr = new [] { 1, 2, 3, 4, 5 };
-fun(arr);
+Fun(arr);
+
+int ArrSum(int[] arr)
+{
+    int sum = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+Console.WriteLine("Sum equals " + ArrSum(arr));
